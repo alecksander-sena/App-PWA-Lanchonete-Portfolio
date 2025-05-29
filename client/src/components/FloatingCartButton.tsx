9f169056d30a103a8112629dac97562f06c027b9
Alecksander de Sena Silva <alecksander.sena@gmail.com>
2025-05-29 10:05:52 -0300

    button car mobile

--- client/src/components/FloatingCartButton.tsx
+++ client/src/components/FloatingCartButton.tsx
@@ -9,7 +9,7 @@ export default function FloatingCartButton({ onClick }: { onClick: () => void })
     >
       <img src="/carrinho.png" alt="Carrinho" className="h-8 w-8 object-contain" />
       {totalItems > 0 && (
-        <span className="absolute top-0 right-0 bg-[#af1a2d] text-white text-xs     rounded-full h-5 w-5 flex items-center justify-center font-bold border-2 border-white" style={{transform: 'translate(50%,-50%)'}}>
+        <span className="absolute top-0 right-0 bg-[#af1a2d] text-white text-xs     rounded-full h-5 w-5 flex items-center justify-center font-bold border-2 border-white">
           {totalItems}
         </span>
       )}
